262b3bc853bba4145b9307c170e739c8de1d7afc
shuklina <lera.shuklina@jet.dev>
2022-01-05 10:03:31 +0200

    [RGTC-1875] Replace drupal_get_path

--- src/PDFGenerator.php
+++ src/PDFGenerator.php
@@ -24,7 +24,7 @@ class PDFGenerator {
     ini_set("pcre.backtrack_limit", "5000000");
     $html = mb_convert_encoding(render($settings['body']), 'UTF-8', 'UTF-8');
     $mpdf = new \Mpdf\Mpdf(['format' => 'A4-L', 'tempDir' => $temporary_directory]);
-    $stylesheet = file_get_contents(drupal_get_path('module', 'openy_repeat') . '/css/print_pdf.css');
+    $stylesheet = file_get_contents(\Drupal::service('extension.list.module')->getPath('openy_repeat') . '/css/print_pdf.css');
     $mpdf->SetTitle($settings['title']);
     $mpdf->SetHTMLFooter(render($settings['footer']));
     $mpdf->WriteHTML($stylesheet, 1);
