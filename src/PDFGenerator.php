928a661ee6a8599d94781c5c7f0215c7800362de
Andrii Podanenko <podarokua@gmail.com>
2023-05-10 17:53:33 +0300

    fix: Update PDFGenerator.php

--- src/PDFGenerator.php
+++ src/PDFGenerator.php
@@ -24,11 +24,11 @@ class PDFGenerator {
     $temporary_directory = \Drupal::service('file_system')->getTempDirectory();
     define("_MPDF_TEMP_PATH", $temporary_directory);
     ini_set("pcre.backtrack_limit", "5000000");
-    $html = mb_convert_encoding(render($settings['body']), 'UTF-8', 'UTF-8');
+    $html = mb_convert_encoding(\Drupal::service('renderer')->render($settings['body']), 'UTF-8', 'UTF-8');
     $mpdf = new Mpdf(['format' => 'A4-L', 'tempDir' => $temporary_directory]);
     $stylesheet = file_get_contents(\Drupal::service('extension.list.module')->getPath('openy_repeat') . '/css/print_pdf.css');
     $mpdf->SetTitle($settings['title']);
-    $mpdf->SetHTMLFooter(render($settings['footer']));
+    $mpdf->SetHTMLFooter(\Drupal::service('renderer')->render($settings['footer']));
     $mpdf->WriteHTML($stylesheet, 1);
     $mpdf->WriteHTML($html, 2);
     $download_title = array_key_first($settings["body"]["#content"]["result"]) . ' ';
